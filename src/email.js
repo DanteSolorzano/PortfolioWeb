8b386cb124595df321cba90ba20779b32f9cb822
DanteSolorzano <solorzanodante19@gmail.com>
2025-07-07 16:49:14 -0600

    feat: Start with the email sending service

---
+++ src/email.js
@@ -0,0 +1,9 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : "Contact-Email",
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_ffczvhd", "template_hujqfn7",parms).then(alert("Email sent succesfully"))
+}
