3be81eca47e8ecf0fe8ade5fec9acb6ee3fe3ff1
DanteSolorzano <solorzanodante19@gmail.com>
2025-07-07 16:57:06 -0600

    feat: i made a couple changes on the email section

--- src/email.js
+++ src/email.js
@@ -3,7 +3,7 @@ function sendMail(){
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
         subject : "Contact-Email",
-        message : document.getElementById("message").value
+        message : document.getElementById("message").value,
     }
     emailjs.send("service_ffczvhd", "template_hujqfn7",parms).then(alert("Email sent succesfully"))
 }
